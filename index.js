13468eda53c6e95a85c9e065645c6ca259991567
tntamim037 <tntamim037n@gmail.com>
2025-11-12 18:16:57 +0600

    'six'

--- index.js
+++ index.js
@@ -110,6 +110,16 @@ async function run() {
 });
    
 
+app.get("/my-reviews", verifyToken, async (req, res) => {
+  const email = req.query.email
+  const result = await reviewsColl.find({ userEmail: email }).toArray()
+  res.send(result)
+});
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
